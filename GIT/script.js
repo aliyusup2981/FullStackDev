d723337cee37b40301cec56bd25b624b0a6d1676
Ali Yusup <aliyusup2981@gmail.com>
2025-01-14 22:42:43 +0700

    Learn to add new code then modify status and see what changed from the previous code with [git diff] then [git reset] to return to the previous code

--- GIT/script.js
+++ GIT/script.js
@@ -1,6 +1,8 @@
-/*
-  This is just a sample project for my Git crash course. It doesn't do anything.
-*/
+const add = (a, b) => a +b;
+console.log(add(3,5));
+
+const subtract = (a, b) => a - b;
+console.log(subtract(9, 5));
 
 const divide = (a, b) => a / b;
 console.log(divide(4, 2))
